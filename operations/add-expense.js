84a2d645dcb179f11817544fe828bd185e934b5a
atrociousdinner <nerishcodes@gmail.com>
2025-11-01 22:53:20 +0545

    Add command option. Play with fs module

--- operations/add-expense.js
+++ operations/add-expense.js
@@ -1,18 +1,55 @@
-const { program } = require('commander')
+const { program } = require("commander");
+const { read } = require("fs");
+const fs = require("fs").promises;
+const path = require("path");
 
-const data = {}
-let count = 0
+const filePath = path.join(__dirname, "../database/data.json");
+const data = {};
+const data_array = [];
+let count = 0;
+
+file_status = async () => {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+read_data = async () => {
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+    console.log(data);
+  } catch (err) {
+    console.error(`Cannot read file: `, err);
+  }
+};
+
+write_data = async (data) => {
+  try {
+    await fs.appendFile(filePath, data,"utf8");
+    console.log(data);
+  } catch (err) {
+    console.error(`Cannot read file: `, err);
+  }
+};
 
 program
-    .option('-d, --description <desc>', 'Add a description of the expense')
-    .option('-a, --add <number>', 'Add expense value')
-program.parse() 
-const options = program.opts()
+  .command("add")
+  .option("-d, --description <desc>", "add a description of the expense")
+  .option("-a, --amount <number>", "add expense value")
+  .action(async (options) => {
+    data.id = count++;
+    data.description = options.description;
+    data.amount = options.amount;
+    const status = await file_status();
+    if (status) {
+      null;
+    } else {
+      write_data(JSON.stringify([data], null, 2));
+    }
+  });
 
-for (let key in options) {
-    data['id'] = count
-    data[key] = options[key]
-    count++
-}
-
-console.log(data)
+// file_status();
+program.parse();
