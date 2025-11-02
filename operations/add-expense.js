7fadafc8c9024afb6939bbf74f269cc4b1d3347a
atrociousdinner <nerishcodes@gmail.com>
2025-11-02 17:32:02 +0545

    Finish add functionality

--- operations/add-expense.js
+++ operations/add-expense.js
@@ -1,26 +1,21 @@
 const { program } = require("commander");
-const { read } = require("fs");
 const fs = require("fs").promises;
 const path = require("path");
-
 const filePath = path.join(__dirname, "../database/data.json");
-const data = {};
-const data_array = [];
-let count = 0;
 
-file_status = async () => {
+getExpense = async () => {
   try {
     await fs.access(filePath);
-    return true;
+    return await read_data();
   } catch {
-    return false;
+    return [];
   }
 };
 
 read_data = async () => {
   try {
     const data = await fs.readFile(filePath, "utf8");
-    console.log(data);
+    return data;
   } catch (err) {
     console.error(`Cannot read file: `, err);
   }
@@ -28,10 +23,13 @@ read_data = async () => {
 
 write_data = async (data) => {
   try {
-    await fs.appendFile(filePath, data,"utf8");
-    console.log(data);
+    await fs.writeFile(filePath, data, "utf8");
+    const parsed = (JSON.parse(data))
+    const lastExpense = parsed[parsed.length - 1];
+
+    console.log(`Expense added successfully (ID: ${lastExpense.id})`);
   } catch (err) {
-    console.error(`Cannot read file: `, err);
+    console.error(`Cannot write file: `, err);
   }
 };
 
@@ -40,16 +38,24 @@ program
   .option("-d, --description <desc>", "add a description of the expense")
   .option("-a, --amount <number>", "add expense value")
   .action(async (options) => {
-    data.id = count++;
-    data.description = options.description;
-    data.amount = options.amount;
-    const status = await file_status();
-    if (status) {
-      null;
-    } else {
-      write_data(JSON.stringify([data], null, 2));
+    const data = await getExpense();
+    let id_to_append = 0;
+    let expenses = []
+    if (!(data.length == 0)) {
+      expenses = JSON.parse(data);
+      id_to_append = parseInt(expenses[expenses.length - 1]["id"]);
     }
+    const data_to_send = {};
+
+
+    data_to_send.id = id_to_append + 1;
+    data_to_send.date = new Date().toISOString().split('T')[0]
+    data_to_send.description = options.description;
+    data_to_send.amount = parseInt(options.amount);
+
+
+    expenses.push(data_to_send)
+    write_data(JSON.stringify(expenses, null, 2))
   });
 
-// file_status();
 program.parse();
