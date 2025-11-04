6b3608c91f1f0a84ab0bcd2072ecbd8dc73312e4
atrociousdinner <nerishcodes@gmail.com>
2025-11-04 21:17:02 +0545

    Updated add and list command with category as option.

--- operations/add-expense.js
+++ operations/add-expense.js
@@ -2,31 +2,35 @@ const { program } = require("commander");
 const { write_data } = require("./file_operations/write_data.js");
 const { getExpense } = require("./file_operations/get_expense.js");
 
-
 function add_command(program) {
   program
-  .command("add")
-  .option("-d, --description <desc>", "add a description of the expense")
-  .option("-a, --amount <number>", "add expense value")
-  .action(async (options) => {
-    const data = await getExpense();
-    let id_to_append = 0;
-    let expenses = [];
-    if (!(data.length == 0)) {
-      expenses = JSON.parse(data);
-      id_to_append = parseInt(expenses[expenses.length - 1]["id"]);
-    }
-    const data_to_send = {};
+    .command("add")
+    .option("-d, --description <desc>", "add a description of the expense")
+    .option("-a, --amount <number>", "add expense value")
+    .option(
+      "-c, --category <category>",
+      "add category of your expense, default category is misc"
+    )
+    .action(async (options) => {
+      const data = await getExpense();
+      let id_to_append = 0;
+      let expenses = [];
+      if (!(data.length == 0)) {
+        expenses = JSON.parse(data);
+        id_to_append = parseInt(expenses[expenses.length - 1]["id"]);
+      }
+      const data_to_send = {};
 
-    data_to_send.id = id_to_append + 1;
-    data_to_send.date = new Date().toISOString().split("T")[0];
-    data_to_send.description = options.description;
-    data_to_send.amount = parseInt(options.amount);
+      data_to_send.id = id_to_append + 1;
+      data_to_send.date = new Date().toISOString().split("T")[0];
+      data_to_send.description = options.description;
+      data_to_send.amount = parseInt(options.amount);
+      data_to_send.category = options.category || 'misc'
 
-    expenses.push(data_to_send);
-    await write_data(JSON.stringify(expenses, null, 2))
-    console.log(`Expense added successfully (ID: ${data_to_send.id})`);
-  });
+      expenses.push(data_to_send);
+      await write_data(JSON.stringify(expenses, null, 2));
+      console.log(`Expense added successfully (ID: ${data_to_send.id})`);
+    });
 }
 
-module.exports = {add_command}
+module.exports = { add_command };
--- operations/list-expense.js
+++ operations/list-expense.js
@@ -1,11 +1,23 @@
 const { program } = require("commander");
 const { getExpense } = require("./file_operations/get_expense")
 
-const listexpense = async () => {
+const listexpense = async (category) => {
     try {
         const data = await getExpense();
         const expenses = JSON.parse(data || "[]")
-        console.table(expenses)
+        if (category === undefined) {
+            console.table(expenses)
+        }
+        else {
+            filtered_expenses = expenses.filter(expense => expense.category.toLowerCase() == category.toLowerCase())
+            if (filtered_expenses.length == 0) {
+                console.log(`No categories found for ${category}`)
+                return
+            }
+            else {
+                console.table(filtered_expenses)
+            }
+        }
     }
     catch (err) {
         console.error("Error listing expenses: ", err)
@@ -14,9 +26,12 @@ const listexpense = async () => {
 
 function list_command(program) {
     program
-    .command('list')
+        .command('list')
+        .option('-c, --category <category>', 'filter the expense with categories')
     .description('list all expenses')
-    .action(listexpense)
+        .action((options) => {
+        listexpense(options.category)
+    })
 }
 
 
